2c47ada8855d7a6488ffda8ee627abe53c0f8c84
florian <berthelot_florian@yahoo.fr>
2025-05-05 16:07:48 +0200

    ok

--- 5.brief_jo/app/app.py
+++ 5.brief_jo/app/app.py
@@ -8,26 +8,26 @@ def get_engine():
     return create_engine('postgresql+psycopg2://airflow:airflow@postgres:5432/airflow')  # adapte si nécessaire
 
 # Titre
-st.title("🔎 Requête SQL PostgreSQL")
+st.title("Requête SQL PostgreSQL")
 
 # Zone de saisie SQL
-query = st.text_area("💬 Entrez votre requête SQL :", "SELECT * FROM table_jo LIMIT 10")
+query = st.text_area("Entrez votre requête SQL :", "SELECT * FROM table_jo LIMIT 10")
 
 # Bouton exécution
 if st.button("Exécuter la requête"):
     try:
         # Lecture SQL
         df = pd.read_sql(query, get_engine())
-        st.success(f"✅ Requête exécutée. {len(df)} lignes retournées.")
+        st.success(f"Requête exécutée. {len(df)} lignes retournées.")
         st.dataframe(df)
     except Exception as e:
-        st.error(f"❌ Erreur : {e}")
+        st.error(f"Erreur : {e}")
         
         
     # Bouton de téléchargement
     csv = df.to_csv(index=False).encode('utf-8')
     st.download_button(
-        label="⬇️ Télécharger les résultats au format CSV",
+        label="⬇Télécharger les résultats au format CSV",
         data=csv,
         file_name="resultats_requete.csv",
         mime="text/csv"
